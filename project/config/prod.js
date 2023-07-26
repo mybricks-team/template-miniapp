2a9a1a0b24f78fa0464e3e80193360d0c887876e
liubiao03 <cocolbell@163.com>
2023-07-26 17:24:21 +0800

    feat: h5 update

--- project/config/prod.js
+++ project/config/prod.js
@@ -88,28 +88,60 @@ module.exports = {
      * WebpackChain 插件配置
      * @docs https://github.com/neutrinojs/webpack-chain
      */
-    // webpackChain (chain) {
-    //   /**
-    //    * 如果 h5 端编译后体积过大，可以使用 webpack-bundle-analyzer 插件对打包体积进行分析。
-    //    * @docs https://github.com/webpack-contrib/webpack-bundle-analyzer
-    //    */
-    //   chain.plugin('analyzer')
-    //     .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin, [])
+    webpackChain (chain) {
+      chain.optimization.minimize(true)
 
-    //   /**
-    //    * 如果 h5 端首屏加载时间过长，可以使用 prerender-spa-plugin 插件预加载首页。
-    //    * @docs https://github.com/chrisvfritz/prerender-spa-plugin
-    //    */
-    //   const path = require('path')
-    //   const Prerender = require('prerender-spa-plugin')
-    //   const staticDir = path.join(__dirname, '..', 'dist')
-    //   chain
-    //     .plugin('prerender')
-    //     .use(new Prerender({
-    //       staticDir,
-    //       routes: [ '/pages/index/index' ],
-    //       postProcess: (context) => ({ ...context, outputPath: path.join(staticDir, 'index.html') })
-    //     }))
-    // }
+
+      const taroBaseReg = /@tarojs[\\/][a-z]+/;
+      chain.optimization.splitChunks({
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          common: {
+            name: 'common',
+            minChunks: 2,
+            priority: 1
+          },
+          vendors: {
+            name: 'vendors',
+            minChunks: 2,
+            test: module => {
+              return /[\\/]node_modules[\\/]/.test(module.resource)
+            },
+            priority: 10
+          },
+          taro: {
+            name:'taro',
+            test: module => {
+              return taroBaseReg.test(module.context)
+            },
+            priority: 100
+          }
+        }
+      })
+
+      // /**
+      //  * 如果 h5 端编译后体积过大，可以使用 webpack-bundle-analyzer 插件对打包体积进行分析。
+      //  * @docs https://github.com/webpack-contrib/webpack-bundle-analyzer
+      //  */
+      // chain.plugin('analyzer')
+      //   .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin, [])
+
+      // /**
+      //  * 如果 h5 端首屏加载时间过长，可以使用 prerender-spa-plugin 插件预加载首页。
+      //  * @docs https://github.com/chrisvfritz/prerender-spa-plugin
+      //  */
+      // const path = require('path')
+      // const Prerender = require('prerender-spa-plugin')
+      // const staticDir = path.join(__dirname, '..', 'dist')
+      // chain
+      //   .plugin('prerender')
+      //   .use(new Prerender({
+      //     staticDir,
+      //     routes: [ '/pages/index/index' ],
+      //     postProcess: (context) => ({ ...context, outputPath: path.join(staticDir, 'index.html') })
+      //   }))
+    }
   },
 };
